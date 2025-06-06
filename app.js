69c73bdaa8ac245f03ce35827c129dfd2f2e8d5a
Abishekvit <2ravi5abi@gmail.com>
2025-06-06 19:48:29 +0530

    Final changes 1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let navbar=document.querySelector(".navbar");
 let netflixavatar=document.querySelector(".netflix-avatar");
 let body=document.querySelector("body");
-
+let downbar=document.querySelector(".down-bar");
 // Description
 window.mi = "Ethan Hunt and his IMF team must stop a global catastrophe. A gripping race against time packed with action and betrayal.";
 window.mh = "Eight thieves take hostages and lock themselves in the Royal Mint of Spain as a criminal mastermind manipulates the police.";
@@ -15,6 +15,13 @@ window.td = "An international crisis forces a seasoned diplomat into a high-stak
 window.si = "In a world of myth and mystery, Sikandar must rise to reclaim his legacy and restore balance.";
 window.ch = "Based on true events, this historical epic chronicles the bravery and sacrifice of a legendary warrior.";
 window.ja = "A man with a mysterious past sets out on a mission to right societal wrongs, confronting enemies from both his past and present.";
+window.oz = "A financial advisor is forced into laundering money for a drug cartel, pulling his family into a deadly spiral.";
+window.ab = "Trapped in an abandoned Tokyo, players must survive deadly games to return to reality — or die trying.";
+window.bb = "A high school chemistry teacher turned meth kingpin transforms into one of TV’s most iconic antiheroes.";
+window.nag = "A low-level FBI agent gets pulled into a massive conspiracy involving spies, secrets, and national security.";
+window.bf = "A road rage incident escalates into a dark, twisted feud that consumes two strangers’ lives.";
+window.sg = "A Mumbai police officer uncovers a plot that could destroy the city, setting off a race against time and fate.";
+
 
 window.addEventListener("scroll",()=>{
     if(window.scrollY>0){
@@ -25,33 +32,35 @@ window.addEventListener("scroll",()=>{
     }
 });
 new_downbar=()=>{
-    let downbar=document.createElement("div");
-    downbar.classList.add("down-bar");
-    downbar.innerHTML=`
-                    <div class="profile-box">
-                    <div>
-                        <img src="greensmiley.jpg" alt="Profile image" class="profile-icon">
-                        <a href="#" class="nav-link">Astronaut_Nitrolegend</a>
-                    </div>
-                    <div>
-                        <img src="bluesmiley.jpg" alt="Profile image" class="profile-icon">
-                        <a href="#" class="nav-link">Children</a>
-                    </div>
-                    </div>
-                    <div><a href="#" class="nav-link">Profile</a></div>
-                    <div><a href="#" class="nav-link">Logout</a></div>
-                    <div><a href="#" class="nav-link">DEF</a></div>
-                    <div><a href="#" class="nav-link">ABC</a></div>
-                    <div><a href="#" class="nav-link">ABC</a></div>
-                `
-    body.append(downbar);
+    downbar.classList.add("opacity-downbar");
 };
+downbar_remove=()=>{
+    downbar.classList.remove("opacity-downbar");
+}
+let timeout_id=null;
+let check=[1];
 netflixavatar.addEventListener("mouseenter",()=>{
     new_downbar();
 });
 netflixavatar.addEventListener("mouseleave",()=>{
-    let downbar=document.querySelector(".down-bar");
-    downbar.remove();
+downbar_remove();
+check.length=0;
+let timeout_id=setTimeout(()=>{
+    check.push(1);
+},1000)
+});
+downbar.addEventListener("mouseenter",()=>{
+    if(timeout_id){
+        clearTimeout(timeout_id);
+        timeout_id= null;
+    }
+    if(check.length==0){
+        new_downbar();
+    }
+});
+downbar.addEventListener("mouseleave",()=>{
+    check.push(1);
+    downbar_remove();
 })
 
 window.addEventListener("scroll",()=>{
@@ -66,6 +75,10 @@ window.addEventListener("scroll",()=>{
 
 const profileIcon= document.querySelector(".profile-icon");
 profileIcon.addEventListener("click",()=>{
+    window.location.href ="profile-details.html";
+});
+const accountIcon=document.querySelector(".account-display");
+accountIcon.addEventListener("click",()=>{
     window.location.href ="profile-details.html";
 });
 
@@ -89,5 +102,9 @@ movieCard.forEach((card)=>{
         hero.style.backgroundSize = "cover";
         hero.style.backgroundRepeat = "no-repeat";
         herodescription.innerText=window[card.id];
+        window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Optional: makes the scroll smooth
+        });
     })
 })
